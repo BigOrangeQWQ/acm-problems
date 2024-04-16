ce26c6d75a6ba6dbab9e989a815bf3b616149491
BigOrangeQWQ <2284086963@qq.com>
2024-04-16 09:32:31 +0800

    update

--- CF1954/E.cpp
+++ CF1954/E.cpp
@@ -19,24 +19,34 @@ using PII = pair<int, int>;
 using LL = long long;
 
 void solve() {
-    int n, m, k;
-    cin >> n >> m >> k;
-    vector<int> x(m);
-    for(int i = 1, cnt = 0; i <= n;i ++) {
-        x[cnt] ++; 
-        cnt = (cnt + 1) % m;
+    int n, m = 0;
+    cin >> n;
+    vector<int> p(n + 1);
+    for(int i = 1; i <= n;i ++) {
+        cin >> p[i];
+        m += p[i];
     }
-    sort(x.begin(), x.end());
-    if(k < n - x.back()) {
-        cout << "YES" << endl;
+    sort(p.begin() + 1, p.end());
+    vector<int> dp(m + 1);
+    dp[0] = 1;
+    LL res = 0;
+    for(int i = 1; i <= n;i ++) {
+        for(int j = 0;j <= m;j ++) {
+            if(p[i] > j)
+                res += dp[j] * p[i];
+            else 
+                res += dp[j] * ((p[i] + j + 1) / 2);
+        }
+        for(int j = m; j >= dp[i]; j--) {
+            dp[j] += dp[j - dp[i]];
+        }
     }
-    else 
-        cout << "NO" << endl;
+    cout << res << endl;
 }
 
 signed main() {
-    int T;cin >> T;
-    // int T = 1;
+    // int T;cin >> T;
+    int T = 1;
     while(T --> 0) {
         solve();
     }
